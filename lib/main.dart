af803bc851837b833370d1fcfdf07358ec1d37be
Sam Espinoza <sespino3@stumail.jccc.edu>
2022-12-12 10:53:58 -0600

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'blocs/color/color_bloc.dart';
 import 'blocs/counter/counter_bloc.dart';
+import 'observers/color_bloc_observer.dart';
 
 void main() {
+  Bloc.observer = ColorBlocObserver();
   runApp(const MyApp());
 }
 
